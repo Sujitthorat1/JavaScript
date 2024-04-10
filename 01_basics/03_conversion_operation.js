8338a8b8ec808febf0a561c24cef5663e124ca38
Sujit Thorat <136691045+Sujitthorat1@users.noreply.github.com>
2024-04-10 12:28:37 +0000

    conversion completed

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,33 @@
+let score = "33efr";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" =>NaN
+
+//true => 1; false=>0;
+
+
+let isLoggedIn = 12;
+
+let convertedValOfTrue = Boolean(isLoggedIn);
+
+// console.log(convertedValOfTrue);
+// 1  =>true; 0=>false
+// "" =>false
+// "sujit" => true
+
+let someNumber = 13;
+let convertedVal = String(someNumber);
+console.log(convertedVal);
+console.log(typeof convertedVal);
+
+
+
