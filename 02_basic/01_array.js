3efa83e98f2ab5604689a18adae3c46243785bf0
Sujit Thorat <136691045+Sujitthorat1@users.noreply.github.com>
2024-04-22 06:25:03 +0000

    array_01 completed

---
+++ 02_basic/01_array.js
@@ -0,0 +1,47 @@
+//  Array
+
+const myArray = [0, 1, 2, 3, 4, 5, 6];
+// console.log(myArray)
+// const myHeros =[" Thor", 'S']; 
+// console.log(myHeros)
+
+// const additionOfTwoArr = myArray+myHeros;
+
+// console.log(additionOfTwoArr);
+// console.log(typeof(additionOfTwoArr));
+
+// Array methods
+
+// myArray.push(7)
+// myArray.push(8)
+// myArray.pop() //remove last value
+
+// myArray.unshift(0)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3))
+
+
+// const newArr = myArray.join();
+
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+// ######## Slice, Splice ##########
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1,3);
+console.log(myn1);
+console.log("B ", myArray);
+
+
+const myn2 = myArray.splice(1,3);
+console.log("c ", myArray);
+console.log(myn2);
+// console.log("myn2 ", myArray);
+
+
+
